6cc1459745bc81d033b04f4d82f23248772ba165
Andreas Braun <andreas.braun@mongodb.com>
2024-06-03 15:35:25 +0200

    Add action to export code scanning alert to a SARIF file (#11)
    Co-authored-by: Steven Silvester <steven.silvester@ieee.org>
    Co-authored-by: Bailey Pearson <bailey.pearson@gmail.com>
    Co-authored-by: Steven Silvester <steve.silvester@mongodb.com>

---
+++ code-scanning-export/src/main.ts
@@ -0,0 +1,65 @@
+import * as core from '@actions/core'
+import { getAlerts } from './api'
+import { createSarifReport } from './sarif'
+import * as fs from 'fs'
+import * as process from 'process'
+import * as path from 'path'
+
+type RepositoryInfo = {
+  owner: string
+  repo: string
+}
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  const repositoryInfo = getRepositoryInfo()
+  const ref = core.getInput('ref')
+
+  core.info(
+    `Fetching open and dismissed alerts for repository ${repositoryInfo.owner}/${repositoryInfo.repo}#${ref}`
+  )
+
+  const alerts = await getAlerts(
+    repositoryInfo.owner,
+    repositoryInfo.repo,
+    ref,
+    core.getInput('token')
+  )
+
+  core.info(`Found ${alerts.length} alerts, processing now...`)
+
+  const sarifReport = createSarifReport(alerts)
+  const filePath = path.join(process.cwd(), core.getInput('output-file'))
+
+  core.info(`Processing done, writing report to file ${filePath}`)
+
+  fs.writeFileSync(filePath, JSON.stringify(sarifReport), {})
+}
+
+function getRepositoryInfo(): RepositoryInfo {
+  const repository = process.env['GITHUB_REPOSITORY']
+  if (repository === undefined) {
+    throw new Error('"GITHUB_REPOSITORY" environment variable must be set')
+  }
+
+  return parseRepositoryInfo(repository)
+}
+
+/**
+ * Shamelessly stolen from https://github.com/github/codeql-action/blob/acdf23828ad6151dd966f467acc7cf231aca129b/src/repository.ts#L9
+ */
+function parseRepositoryInfo(input: string): RepositoryInfo {
+  const parts = input.split('/')
+
+  if (parts.length !== 2) {
+    throw new Error(`"${input}" is not a valid repository name`)
+  }
+
+  return {
+    owner: parts[0],
+    repo: parts[1]
+  }
+}
---
+++ code-scanning-export/src/sarif.ts
@@ -0,0 +1,169 @@
+/* eslint import/no-unresolved: 0 */
+import { components } from '@octokit/openapi-types'
+
+export type AlertType = components['schemas']['code-scanning-alert-items']
+export type RuleType = components['schemas']['code-scanning-alert-rule-summary']
+export type AlertLocationType =
+  components['schemas']['code-scanning-alert-location']
+
+type SarifReport = {
+  version: string
+  $schema: string
+  runs: {
+    tool: {
+      driver: {
+        name: string
+        version: string | null | undefined
+        rules: object[]
+      }
+    }
+    results: object[]
+  }[]
+}
+
+type Region = {
+  startLine?: number
+  endLine?: number
+  startColumn?: number
+  endColumn?: number
+}
+
+export function createSarifReport(alerts: AlertType[]): SarifReport {
+  const results: {
+    [index: string]: {
+      tool: {
+        driver: {
+          name: string
+          version: string | null | undefined
+          rules: { [index: string]: object }
+        }
+      }
+      results: object[]
+    }
+  } = {}
+
+  for (const alert of alerts) {
+    if (!alert.tool.name) {
+      continue
+    }
+
+    if (!results[alert.tool.name]) {
+      results[alert.tool.name] = {
+        tool: {
+          driver: {
+            name: alert.tool.name,
+            version: alert.tool.version,
+            rules: {}
+          }
+        },
+        results: []
+      }
+    }
+
+    results[alert.tool.name].results.push(createSarifResult(alert))
+
+    const ruleName = getRuleIdentifier(alert.rule)
+
+    if (ruleName && !results[alert.tool.name].tool.driver.rules[ruleName]) {
+      results[alert.tool.name].tool.driver.rules[ruleName] = createSarifRule(
+        alert.rule
+      )
+    }
+  }
+
+  return {
+    version: '2.1.0',
+    $schema: 'https://json.schemastore.org/sarif-2.1.0.json',
+    runs: Object.keys(results).map((toolName: string) => {
+      const toolResults = results[toolName]
+
+      return {
+        tool: {
+          driver: {
+            ...toolResults.tool.driver,
+            rules: Object.keys(toolResults.tool.driver.rules).map(
+              ruleName => toolResults.tool.driver.rules[ruleName]
+            )
+          }
+        },
+        results: toolResults.results
+      }
+    })
+  }
+}
+
+function createSarifRule(rule: RuleType): object {
+  return {
+    id: getRuleIdentifier(rule),
+    shortDescription: { text: rule.description },
+    properties: { tags: rule.tags }
+  }
+}
+
+export function createSarifResult(alert: AlertType): object {
+  return {
+    ruleId: getRuleIdentifier(alert.rule),
+    message: alert.most_recent_instance.message,
+    level: alert.rule.severity,
+    locations: createResultLocation(alert),
+    suppressions: createResultSuppressions(alert)
+  }
+}
+
+function createResultLocation(alert: AlertType): object[] {
+  if (!alert.most_recent_instance.location) {
+    return []
+  }
+
+  return [
+    {
+      physicalLocation: {
+        artifactLocation: { uri: alert.most_recent_instance.location.path },
+        region: createRegion(alert.most_recent_instance.location)
+      }
+    }
+  ]
+}
+
+function createResultSuppressions(alert: AlertType): object[] {
+  if (alert.state !== 'dismissed' || !alert.dismissed_reason) {
+    return []
+  }
+
+  let justification: string = alert.dismissed_reason
+  if (alert.dismissed_comment) {
+    justification += `;${alert.dismissed_comment}`
+  }
+
+  return [
+    {
+      kind: 'external',
+      status: 'accepted',
+      justification
+    }
+  ]
+}
+
+function createRegion(location: AlertLocationType): Region {
+  const region: Region = {}
+
+  if (location.start_line) {
+    region.startLine = location.start_line
+  }
+  if (location.end_line) {
+    region.endLine = location.end_line
+  }
+
+  if (location.start_column) {
+    region.startColumn = location.start_column
+  }
+  if (location.end_column) {
+    region.endColumn = location.end_column
+  }
+
+  return region
+}
+
+function getRuleIdentifier(rule: RuleType): string {
+  return rule.name ? rule.name : rule.id ? rule.id : ''
+}
