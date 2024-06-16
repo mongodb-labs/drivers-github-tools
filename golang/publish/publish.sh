382c51f672346511d9fc743a88b06aff1a685f7b
Steven Silvester <steve.silvester@mongodb.com>
2024-06-16 10:02:30 -0500

    fix notes file handling

--- golang/publish/publish.sh
+++ golang/publish/publish.sh
@@ -13,15 +13,17 @@ go run notes.go $VERSION $PREV_VERSION
 cat forum.md >> $GITHUB_STEP_SUMMARY
 rm forum.md
 
+NOTES_FILE=$(pwd)/github.md
+
 # Handle GitHub Release
 if [ "$PUSH_CHANGES" == "true" ]; then
     pushd $GITHUB_WORKSPACE
-    gh release create ${VERSION} --draft --verify-tag --title ${VERSION} -F github.md
+    gh release create ${VERSION} --draft --verify-tag --title ${VERSION} -F $NOTES_FILE
     gh release upload ${VERSION} $RELEASE_ASSETS/*.*
     gh release view ${VERSION} >> $GITHUB_STEP_SUMMARY
     popd
 else
     echo "## Skipping draft release with notes:" >> $GITHUB_STEP_SUMMARY
-    cat github.md >> $GITHUB_STEP_SUMMARY
+    cat $NOTES_FILE >> $GITHUB_STEP_SUMMARY
 fi
-rm github.md
+rm $NOTES_FILE
