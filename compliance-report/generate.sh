a331c7891a31e98b18f6f3dc35ea1c6f00e0c8e7
Steven Silvester <steve.silvester@mongodb.com>
2024-06-16 11:18:23 -0500

    fix handling of security_report_url

--- compliance-report/generate.sh
+++ compliance-report/generate.sh
@@ -9,13 +9,14 @@ RELEASE_CREATOR=$(gh api users/${GITHUB_ACTOR} --jq '.name')
 
 # Handle security report.
 SECURITY_REPORT="N/A"
-SECURITY_REPORT_LOCATION=
 if [ -n "$SECURITY_REPORT_LOCATION" ]; then
   if [[ $SECURITY_REPORT_LOCATION == https* ]]; then
     SECURITY_REPORT="See $SECURITY_REPORT_LOCATION"
   else
     SECURITY_REPORT="See https://github.com/$GITHUB_REPOSITORY/blob/$RELEASE_VERSION/$SECURITY_REPORT_LOCATION"
   fi
+elif [ -n "$SECURITY_REPORT_URL" ]; then
+    SECURITY_REPORT="See $SECURITY_REPORT_URL"
 fi
 
 cat << EOF >> ${S3_ASSETS}/ssdlc_compliance_report.md
@@ -40,3 +41,5 @@ ${SECURITY_REPORT}
 Known Vulnerabilities
 Any vulnerabilities that may be shown in the files referenced above have been reviewed and accepted by the appropriate approvers.
 EOF
+
+cat ${S3_ASSETS}/ssdlc_compliance_report.md
