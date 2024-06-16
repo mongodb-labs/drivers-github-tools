8058ed2037766623b08729e267e48fe63df9c4c1
Steven Silvester <steve.silvester@mongodb.com>
2024-06-16 16:42:17 -0500

    clean up release

--- golang/publish/publish.sh
+++ golang/publish/publish.sh
@@ -18,9 +18,10 @@ NOTES_FILE=$(pwd)/github.md
 # Handle GitHub Release
 if [ "$PUSH_CHANGES" == "true" ]; then
     pushd $GITHUB_WORKSPACE
-    gh release create ${VERSION} --draft --verify-tag --title v${VERSION} -F $NOTES_FILE
+    TITLE="MongoDB Go Driver ${VERSION}
+    gh release create ${VERSION} --draft --verify-tag --title $TITLE -F $NOTES_FILE
     gh release upload v${VERSION} $RELEASE_ASSETS/*.*
-    gh release view $v{VERSION} >> $GITHUB_STEP_SUMMARY
+    gh release view v${VERSION} >> $GITHUB_STEP_SUMMARY
     popd
 else
     echo "## Skipping draft release with notes:" >> $GITHUB_STEP_SUMMARY
