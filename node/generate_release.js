2f3357e96e10b6dcd546398eaa38b92e7783f2ed
Bailey Pearson <bailey.pearson@mongodb.com>
2024-06-26 10:58:39 -0600

    prettier changes

--- node/generate_release.js
+++ node/generate_release.js
@@ -1,32 +1,44 @@
-const { readFileSync } = require('fs');
-const { join } = require('path');
+const { readFileSync } = require("fs");
+const { join } = require("path");
 
 const args = process.argv.slice(2);
-if (args.length != 3) {
-	console.error(`usage: generate_release.js <package> <branch> <npm tag>`);
-	process.exit(1);
+if (!(args.length === 3 || args.length === 4)) {
+  console.error(
+    `usage: generate_release.js <package> <branch> <npm tag> <optional silk asset group>`,
+  );
+  process.exit(1);
 }
 
-const [package, branch, tag] = args;
+const [package, branch, tag, assetGroup] = args;
 
-const isNative = package === 'kerberos' || package === 'mongodb-client-encryption';
-const template = readFileSync(join(__dirname, isNative ? './native_release_template.yml' : './release_template.yml'), 'utf-8');
+const isNative =
+  package === "kerberos" || package === "mongodb-client-encryption";
+const template = readFileSync(
+  join(__dirname, "./release_template.yml"),
+  "utf-8",
+);
 
 const EVERGREEN_PROJECTS = {
-	'mongodb': 'mongo-node-driver-next',
-	'bson': 'js-bson'
+  mongodb: "mongo-node-driver-next",
+  bson: "js-bson",
 };
 
-const generated = template.replaceAll('RELEASE_BRANCH', branch)
-	.replaceAll('RELEASE_PACKAGE', package)
-	.replaceAll('RELEASE_TAG', tag)
-	.replaceAll('EVERGREEN_PROJECT', EVERGREEN_PROJECTS[package] ?? '');
+const generated = template
+  .replaceAll("RELEASE_BRANCH", branch)
+  .replaceAll("RELEASE_PACKAGE", package)
+  .replaceAll("RELEASE_TAG", tag)
+  .replaceAll("EVERGREEN_PROJECT", EVERGREEN_PROJECTS[package] ?? "")
+  .replaceAll("IGNORE_INSTALL_SCRIPTS", isNative)
+  .replaceAll("SILK_ASSET_GROUP", assetGroup ?? "''");
 
 const project = EVERGREEN_PROJECTS[package];
 if (!project) {
-	const final = generated.split('\n').filter(line => !line.includes("evergreen")).join('\n');
-	process.stdout.write(final);
-	process.exit();
+  const final = generated
+    .split("\n")
+    .filter((line) => !line.includes("evergreen"))
+    .join("\n");
+  process.stdout.write(final);
+  process.exit();
 }
 
 process.stdout.write(generated);
