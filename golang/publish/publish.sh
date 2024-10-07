d7a12d0609616ef0776362ce46db53f0428d03e2
Steven Silvester <steve.silvester@mongodb.com>
2024-10-07 12:18:52 -0500

    Fix handling of cloud tags in go publish

--- golang/publish/publish.sh
+++ golang/publish/publish.sh
@@ -9,8 +9,16 @@ else
 fi
 echo "DRY_RUN=$DRY_RUN" >> $GITHUB_ENV
 
+# Handle cloud release versions
+TAG_NAME="v${VERSION}"
+PREV_TAG_NAME="v${PREV_VERSION}"
+if [[ "${TAG_NAME}" =~ ^cloud.* ]]; then
+  TAG_NAME="${VERSION}"
+  PREV_TAG_NAME="${PREV_VERSION}"
+fi
+
 # Generate notes
-go run notes.go $VERSION $PREV_VERSION
+go run notes.go $TAG_NAME $PREV_TAG_NAME
 cat forum.md >> $GITHUB_STEP_SUMMARY
 rm forum.md
 
@@ -22,12 +30,12 @@ NOTES_FILE=$(pwd)/github.md
 if [ "$PUSH_CHANGES" == "true" ]; then
     pushd $GITHUB_WORKSPACE || exit 1
     TITLE="MongoDB Go Driver ${VERSION}"
-    gh release create v${VERSION} --draft --verify-tag --title "$TITLE" -F $NOTES_FILE
-    gh release upload v${VERSION} $RELEASE_ASSETS/*.*
+    gh release create ${TAG_NAME} --draft --verify-tag --title "$TITLE" -F $NOTES_FILE
+    gh release upload ${TAG_NAME} $RELEASE_ASSETS/*.*
     JSON="url,tagName,assets,author,createdAt"
     JQ='.url,.tagName,.author.login,.createdAt,.assets[].name'
     echo "\## $TITLE" >> $GITHUB_STEP_SUMMARY
-    gh release view --json $JSON --jq $JQ v${VERSION} >> $GITHUB_STEP_SUMMARY
+    gh release view --json $JSON --jq $JQ ${TAG_NAME} >> $GITHUB_STEP_SUMMARY
     popd || exit 1
 else
     echo "## Skipping draft release with notes:" >> $GITHUB_STEP_SUMMARY
